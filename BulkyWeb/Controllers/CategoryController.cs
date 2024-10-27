4589cde164397a71cd9a3ebc583d475640553b39
annisarisma <annisarisma117@gmail.com>
2024-10-28 01:45:12 +0700

    add: db conneection and post data from controller

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -7,6 +7,7 @@ namespace BulkyWeb.Controllers
     public class CategoryController : Controller
     {
         private readonly ApplicationDbContext _db;
+
         public CategoryController(ApplicationDbContext db)
         {
             _db = db;
@@ -22,5 +23,13 @@ namespace BulkyWeb.Controllers
         {
             return View();
         }
-    }
+
+        [HttpPost]
+		public IActionResult Create(Category obj)
+		{
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
 }
