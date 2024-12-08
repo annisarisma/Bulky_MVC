85ed6c4400c51f2191ed36a491fd577e52d8c513
annisarisma <annisarisma117@gmail.com>
2024-12-08 18:02:00 +0700

    add: delete category

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -41,5 +41,65 @@ namespace BulkyWeb.Controllers
 
             return View();
 		}
-	}
+
+		public IActionResult Edit(int? id)
+		{
+            if (id == null || id == 0) {
+                return NotFound();
+            }
+
+            Category? data = _db.Categories.Find(id);
+
+			if (data == null) {
+                return NotFound();
+            }
+
+            return View(data);
+		}
+
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Update(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+
+			return View();
+		}
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? data = _db.Categories.Find(id);
+
+            if (data == null)
+            {
+                return NotFound();
+            }
+
+            return View(data);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+
+            if (obj == null) {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }
 }
