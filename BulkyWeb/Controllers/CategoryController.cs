1647243faf101659a9345d0a8e3f58c7af7b727d
annisarisma <annisarisma117@gmail.com>
2024-12-08 18:19:44 +0700

    add: notification when success with TempData

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -36,7 +36,8 @@ namespace BulkyWeb.Controllers
             {
 				_db.Categories.Add(obj);
 				_db.SaveChanges();
-				return RedirectToAction("Index");
+                TempData["success"] = "Category created successfully";
+				return RedirectToAction("Index", TempData);
 			}
 
             return View();
@@ -64,7 +65,9 @@ namespace BulkyWeb.Controllers
 			{
 				_db.Categories.Update(obj);
 				_db.SaveChanges();
-				return RedirectToAction("Index");
+				TempData["success"] = "Category updated successfully";
+
+				return RedirectToAction("Index", TempData);
 			}
 
 			return View();
@@ -98,7 +101,8 @@ namespace BulkyWeb.Controllers
 
             _db.Categories.Remove(obj);
             _db.SaveChanges();
-            return RedirectToAction("Index");
+			TempData["success"] = "Category deleted successfully";
+			return RedirectToAction("Index", TempData);
 
         }
     }
